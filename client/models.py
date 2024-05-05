ba24c9b782090fb2b7f534ccce262e85113efe2a
bmamaxim <bma@gmail.com>
2024-05-05 13:01:05 +0500

    client

--- client/models.py
+++ client/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name='элктронная почта')
+    name = models.CharField(max_length=300, verbose_name='Ф.И.О', **NULLABLE)
+    comment = models.CharField(verbose_name='комментарий', **NULLABLE)
+    publication_sign = models.BooleanField(default=True, verbose_name='публикация')
+    created_at = models.DateField(auto_now_add=True, verbose_name='дата записи', **NULLABLE)
+    updated_at = models.DateField(auto_now_add=True, verbose_name='последние изменения', **NULLABLE)
+
+
+    def __str__(self):
+        return (f'{self.email}'
+                f'{self.name}')
+
+    class Meta:
+
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
