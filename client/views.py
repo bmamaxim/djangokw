7d791294d787275ce56e4eca2b36d13e5946726b
bmamaxim <bma@gmail.com>
2024-05-10 20:45:37 +0500

    client

--- client/views.py
+++ client/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from client.forms import ClientForm
 from client.models import Client
@@ -16,4 +16,23 @@ class ClientCreateView(CreateView):
 
     model = Client
     form_class = ClientForm
+    success_url = reverse_lazy('client:home')
+
+
+class ClientDetailView(DetailView):
+
+    model = Client
+    template_name = 'client/client_detail.html'
+
+
+class ClientUpdateView(UpdateView):
+
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('client:home')
+
+
+class ClientDeleteView(DeleteView):
+
+    model = Client
     success_url = reverse_lazy('client:home')
